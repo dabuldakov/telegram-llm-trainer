85affbfda50ad9fe7b6d42b917a9282b97a9bc5b
Булдаков Дмитрий Александрович <dmitriy.buldakov@rosbank.ru>
2025-07-06 21:31:50 +0700

    fix

--- bot/chat_model.py
+++ bot/chat_model.py
@@ -1,16 +1,13 @@
 from transformers import (
     AutoModelForCausalLM,
-    AutoTokenizer,
-    GenerationConfig
+    AutoTokenizer
 )
 import torch
 import datetime
 from config import Config
 
-model_name = "mistralai/Mistral-7B-v0.1"
 model_path = Config.MODEL_PATH_FINISHED_TRAIN
 logs_dir = Config.TRAINING_LOGS_PATH
-generation_config = GenerationConfig.from_pretrained(model_name)
 
 class ChatModel:
     def __init__(self):
@@ -43,14 +40,22 @@ class ChatModel:
         return output.replace("</|assistant|>", "").strip()
     
     def generate_summury(self, prompt):
-        data = self.tokenizer(prompt, return_tensors="pt", add_special_tokens=False)
-        data = {k: v.to(self.model.device) for k, v in data.items()}
+        inputs = self.tokenizer(prompt, return_tensors="pt", add_special_tokens=True).to("cuda")
+        data = {k: v.to(self.model.device) for k, v in inputs.items()}
         output_ids = self.model.generate(
             **data,
-            generation_config=generation_config
+            max_new_tokens=256,
+            do_sample=True,
+            temperature=0.7,
+            top_p=0.9,        # 0.8-0.95 (nucleus sampling)
+            top_k=50,         # Ограничивает выбор топ-K токенов
+            repetition_penalty=1.2,  # Штраф за повторения (1.0-2.0)
         )[0]
+
+        self.log_output_ids(output_ids, data)
+
         output_ids = output_ids[len(data["input_ids"][0]):]
-        output = self.tokenizer.decode(output_ids, skip_special_tokens=True)
+        output = self.tokenizer.decode(output_ids)
         return output.strip()
     
     def log_output_ids(self, output_ids, data):
